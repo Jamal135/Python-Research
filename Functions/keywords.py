dd86133747fd80727442fb65f8e67344df95a73c
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-11-15 13:36:10 +1000

    Started keyword testing

--- Functions/keywords.py
+++ Functions/keywords.py
@@ -1,1 +1,21 @@
-''''''
+''' Creation Date: 15/11/2022 '''
+
+from keybert import KeyBERT
+
+
+MODEL = KeyBERT(model = 'all-mpnet-base-v2')
+
+
+def get_keywords(text: str):
+    ''' Returns: List of keywords for given string. '''
+    keywords = MODEL.extract_keywords(
+        text,
+        keyphrase_ngram_range = (1, 1),
+        stop_words = 'english',
+        highlight = False,
+        top_n = 5
+    )
+    return list(dict(keywords).keys())
+
+
+print(get_keywords('''The flexibility to apply regulatory approaches based on the risk'''))
